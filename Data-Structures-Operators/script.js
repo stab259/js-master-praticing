9fd429c5c42e05cf2f8735f3d4914087a6663883
stab259 <nhientran2509@gmail.com>
2022-07-13 08:43:09 +0700

    Enhanced object literals

--- Data-Structures-Operators/script.js
+++ Data-Structures-Operators/script.js
@@ -1,25 +1,38 @@
 'use strict'
 
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 12 + 12,
+  }
+};
+
 const restaurant = {
   name: 'The best PIZAA',
   location: 'Danang city',
   categories: ['Sun', 'Cloud', 'Rain', 'Plant'],
   starterMenu: ['Orange', 'Strawberry', 'Pineapple', 'Mango'],
   mainMenu: ['Red Pizza', 'Blue Pizza', 'Yellow Pizza', 'Black Pizza'],
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    }
-  },
+
+  // ES6 enhanced object literals
+  openingHours,
+
+  // old ways
+  // order: function(starterIndex, mainIndex) {
+  //   return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  // },
+
+  // new ways
   order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
@@ -35,6 +48,10 @@ const restaurant = {
   }
 };
 
+
+
+
+/*
 ///////////////////////////////////////
 // Looping Arrays: The for-of Loop
 
@@ -47,6 +64,7 @@ for (const [i, el] of menu.entries()) {
 }
 
 console.log([...menu.entries()]);
+*/
 
 
 console.log('CODING CHALLENGE #1');
@@ -66,82 +84,84 @@ Suppose we get data from a web service about a certain game (below). In this cha
 TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
 GOOD LUCK 😀
 */
+/*
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
 
-// const game = {
-//   team1: 'Bayern Munich',
-//   team2: 'Borrussia Dortmund',
-//   players: [
-//     [
-//       'Neuer',
-//       'Pavard',
-//       'Martinez',
-//       'Alaba',
-//       'Davies',
-//       'Kimmich',
-//       'Goretzka',
-//       'Coman',
-//       'Muller',
-//       'Gnarby',
-//       'Lewandowski',
-//     ],
-//     [
-//       'Burki',
-//       'Schulz',
-//       'Hummels',
-//       'Akanji',
-//       'Hakimi',
-//       'Weigl',
-//       'Witsel',
-//       'Hazard',
-//       'Brandt',
-//       'Sancho',
-//       'Gotze',
-//     ],
-//   ],
-//   score: '4:0',
-//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-//   date: 'Nov 9th, 2037',
-//   odds: {
-//     team1: 1.33,
-//     x: 3.25,
-//     team2: 6.5,
-//   },
-// };
+// 1.
+const [players1, players2] = game.players;
+console.log(players1, players2);
 
-// // 1.
-// const [players1, players2] = game.players;
-// console.log(players1, players2);
+// 2. 
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
 
-// // 2. 
-// const [gk, ...fieldPlayers] = players1;
-// console.log(gk, fieldPlayers);
+// 3.
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
 
-// // 3.
-// const allPlayers = [...players1, ...players2];
-// console.log(allPlayers);
+// 4.
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Periscic'];
 
-// // 4.
-// const players1Final = [...players1, 'Thiago', 'Coutinho', 'Periscic'];
+// 5.
+const {
+  odd: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
 
-// // 5.
-// const {
-//   odd: { team1, x: draw, team2 },
-// } = game;
-// console.log(team1, draw, team2);
+// 6.
+const printGoals = function (...players) {
+  console.log(players);
+  console.log(`${players.length} goals were scored`);
+};
 
-// // 6.
-// const printGoals = function (...players) {
-//   console.log(players);
-//   console.log(`${players.length} goals were scored`);
-// };
+// printGoals('Davies', 'Muller', 'Lewadowski', 'Kimich');
+// printGoals('Davies', 'Muller');
+printGoals(...game.scored);
 
-// // printGoals('Davies', 'Muller', 'Lewadowski', 'Kimich');
-// // printGoals('Davies', 'Muller');
-// printGoals(...game.scored);
+// 7.
+team1 < team2 && console.log('Team 1 is more likely to win');
+team1 > team2 && console.log('Team 2 is more likely to win');
+*/
 
-// // 7.
-// team1 < team2 && console.log('Team 1 is more likely to win');
-// team1 > team2 && console.log('Team 2 is more likely to win');
 
 console.log('LOGICAL ASSIGNMENT OPERATORS');
 /*
